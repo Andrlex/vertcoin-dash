48553cea328af9c8b3d699acb2520942c0bdb607
root <Andrlex>
2017-11-03 19:07:06 +0000

    disclaimer added

---
+++ public/assets/js/config.js
@@ -0,0 +1,13 @@
+
+
+(function ()
+ {
+	'use strict';
+	
+	angular.module('app.constants', [])
+
+.constant('config', {apiEndpoints:{explorer:'http://vertcoin-test-dashboard.com',coinMarket:'https://api.coinmarketcap.com/v1/ticker/vertcoin/'}})
+
+;
+	
+})();
